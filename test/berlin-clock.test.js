701e9e5991cd9a7db33c796ac29f452fdb997f01
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2022-05-16 15:23:54 +0200

    feat: five hour 3rd example featyre test red phase

--- test/berlin-clock.test.js
+++ test/berlin-clock.test.js
@@ -12,6 +12,10 @@ describe('Converting 5-hour', () => {
     test('GIVEN the timestamp is 00:04:03 WHEN coverting to Berlin time THEN the 5-hour row shows ⚫⚫⚫⚫', () => {
         expect(berlinClockFiveHour('00:04:03')).toStrictEqual('⚫⚫⚫⚫');
     });
+
+    test('GIVEN the timestamp is 04:05:23 WHEN coverting to Berlin time THEN the 5-hour row shows ⚫⚫⚫⚫', () => {
+        expect(berlinClockFiveHour('04:05:23')).toStrictEqual('⚫⚫⚫⚫');
+    });
 });
 
 
